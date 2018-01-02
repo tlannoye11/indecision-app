2cfa33bd37e67a81c302d02751fac50dc2135db3
tlannoye11 <travis.lannoye@gmail.com>
2018-01-02 12:39:50 -0600

    New Year's Changes II

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,68 +1,45 @@
 'use strict';
 
-console.log("app.js is running");
+console.log('App.js is running!');
 
-// JSX = JavaScript XML
-var appObject = {
+var app = {
   title: 'Indecision App',
-  subtitle: 'by Travis Lannoye',
+  subtitle: 'Put your life in the hands of a computer',
   options: ['One', 'Two']
 };
-
 var template = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    appObject.title
+    app.title
   ),
-  appObject.subtitle && React.createElement(
+  app.subtitle && React.createElement(
     'p',
     null,
-    appObject.subtitle
+    app.subtitle
   ),
   React.createElement(
     'p',
     null,
-    appObject.options.length > 0 ? 'Here are your options' : 'No options'
+    app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
   )
 );
 
-var user = {
-  name: 'Travis',
-  age: 33,
-  location: 'Madison'
-};
-
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      'p',
-      null,
-      ' Location: ',
-      location
-    );
-  }
-}
-
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    user.name ? user.name : 'Anonymous'
-  ),
-  user.age >= 18 && React.createElement(
-    'p',
-    null,
-    'Age: ',
-    user.age
-  ),
-  getLocation(user.location)
-);
-
 var appRoot = document.getElementById('app');
-
 ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log('App.js is running!');
+
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+const appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
